d4940c95a0adecd1e77f910957e4462c95824bde
khvci <88493840+khvci@users.noreply.github.com>
2022-10-25 20:58:21 +0300

    remove unused getters&setters

--- src/entities/concretes/Employee.java
+++ src/entities/concretes/Employee.java
@@ -57,37 +57,4 @@ public class Employee implements ICalculable {
         this.workHours = workHours;
         this.hireYear = hireYear;
     }
-
-    //getters &  setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public int getWorkHours() {
-        return workHours;
-    }
-
-    public void setWorkHours(int workHours) {
-        this.workHours = workHours;
-    }
-
-    public int getHireYear() {
-        return hireYear;
-    }
-
-    public void setHireYear(int hireYear) {
-        this.hireYear = hireYear;
-    }
 }
